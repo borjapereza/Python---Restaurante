6cf3a83bd627f06fbfda802d799d8315b0c2e7ea
Fonsi20 <fonsi20fernandez@gmail.com>
2018-12-19 10:21:44 +0100

    Muestra de la pantalla Principal

--- Restaurante.py
+++ Restaurante.py
@@ -1,18 +1,32 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+
 import gi
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
-class Calculadora:
+class Restaurante:
 
     def __init__(self):
         b = Gtk.Builder()
         b.add_from_file('Restaurante.glade')
 
+        # Objetos
+        self.venprincipal = b.get_object('VentanaPrincipal')
+
+        # Diccionario
+        # Eventos
+        dic = {'on_VentanaPrincipal_destroy': self.btnsal,
+               }
+
+        self.venprincipal.show()
+
+    def btnsal(self, data=None):
+        Gtk.main_quit()
+
 if __name__ == '__main__':
-     main = Calculadora()
-     Gtk.main()
+    main = Restaurante()
+    Gtk.main()
